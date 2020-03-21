3bc47940d89c077348edf845d1df1dbca646cbb7
Alden Parker <32986873+aldenparker@users.noreply.github.com>
2020-03-21 01:10:13 -0700

    Update save_build.sh

--- save_build.sh
+++ save_build.sh
@@ -4,5 +4,5 @@ dir=$1_build_$(date +"%Y-%m-%d-%I:%M-%p")
 
 mkdir $dir
 
-cp -r ../catkin_ws/build ./$dir/
-cp -r ../catkin_ws/devel ./$dir/
+cp -r $2/catkin_ws/build ./$dir/
+cp -r $2/catkin_ws/devel ./$dir/
