a1d6d33d7ecfb705478caf1b38eb2d7249539eb1
Alden Parker <32986873+aldenparker@users.noreply.github.com>
2020-03-22 13:13:35 -0700

    Update save_build.sh

--- save_build.sh
+++ save_build.sh
@@ -11,7 +11,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 git add .
-git commit -m "Travis Build"
+git commit -m "Travis Build $4"
 
 git remote add master https://$3@github.com/BytesRobotics/br-travis-builds.git > /dev/null 2>&1
 git push --quiet --set-upstream master master
