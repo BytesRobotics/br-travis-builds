47669936005e7cd193e6ad0bc60250e0e8a5dd19
Alden Parker <ajparker1401@gmail.com>
2020-03-21 21:42:29 -0700

    put that thang back in

--- save_build.sh
+++ save_build.sh
@@ -6,3 +6,7 @@ mkdir $dir
 
 cp -r $2/catkin_ws/build ./$dir/
 cp -r $2/catkin_ws/devel ./$dir/
+
+git add .
+git commit -m "Travis Build"
+git push 'https://$3@github.com/BytesRobotics/br-travis-builds.git'
