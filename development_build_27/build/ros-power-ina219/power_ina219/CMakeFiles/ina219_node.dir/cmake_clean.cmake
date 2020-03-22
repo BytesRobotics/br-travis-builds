3823864e666cdb3f53d3ed5a161f749793e22812
Travis CI <travis@travis-ci.org>
2020-03-22 22:29:47 +0000

    Travis Build 27

---
+++ development_build_27/build/ros-power-ina219/power_ina219/CMakeFiles/ina219_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ina219_node.dir/src/ina219_activity.cpp.o"
+  "CMakeFiles/ina219_node.dir/src/ina219_node.cpp.o"
+  "/home/travis/build/BytesRobotics/br-core/catkin_ws/devel/lib/power_ina219/ina219_node.pdb"
+  "/home/travis/build/BytesRobotics/br-core/catkin_ws/devel/lib/power_ina219/ina219_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ina219_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
