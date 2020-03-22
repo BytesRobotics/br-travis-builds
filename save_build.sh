758d18537b729932f103ca4fb3c340a2ef8a4c87
Alden Parker <32986873+aldenparker@users.noreply.github.com>
2020-03-22 12:29:17 -0700

    Update save_build.sh

--- save_build.sh
+++ save_build.sh
@@ -7,6 +7,11 @@ mkdir $dir
 cp -r $2/catkin_ws/build ./$dir/
 cp -r $2/catkin_ws/devel ./$dir/
 
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
 git add .
 git commit -m "Travis Build"
-git push 'https://$3@github.com/BytesRobotics/br-travis-builds.git'
+
+git remote add master https://$3@github.com/BytesRobotics/br-travis-builds.git > /dev/null 2>&1
+git push --quiet --set-upstream master master
