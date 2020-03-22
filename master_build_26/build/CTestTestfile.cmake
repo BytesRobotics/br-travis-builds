bcf1f84e890696e1fa78ab343e02216ac55a457b
Travis CI <travis@travis-ci.org>
2020-03-22 20:46:29 +0000

    Travis Build 26

---
+++ master_build_26/build/CTestTestfile.cmake
@@ -0,0 +1,17 @@
+# CMake generated Testfile for 
+# Source directory: /home/travis/build/BytesRobotics/br-core/catkin_ws/src
+# Build directory: /home/travis/build/BytesRobotics/br-core/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("br-perception")
+subdirs("launch_files")
+subdirs("br-behavior-engine")
+subdirs("ros-power-ina219/power_ina219")
+subdirs("bno055")
+subdirs("duckietown-gazebo/duckietown_gazebo")
+subdirs("br-hardware-interface")
+subdirs("br-simulator")
+subdirs("br-control")
+subdirs("br-description")
---
+++ master_build_26/build/br-behavior-engine/CMakeFiles/_br_behavior_engine_generate_messages_check_deps_Events.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_br_behavior_engine_generate_messages_check_deps_Events"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/_br_behavior_engine_generate_messages_check_deps_Events.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
