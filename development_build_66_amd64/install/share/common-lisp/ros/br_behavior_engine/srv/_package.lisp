1755ce179de7657aa9cf8f9c225f6f0ac725bad1
Travis CI <travis@travis-ci.org>
2020-03-24 20:45:52 +0000

    Travis Build 66

---
+++ development_build_66_amd64/install/share/common-lisp/ros/br_behavior_engine/srv/_package.lisp
@@ -0,0 +1,10 @@
+(cl:defpackage br_behavior_engine-srv
+  (:use )
+  (:export
+   "STRINGLIST"
+   "<STRINGLIST-REQUEST>"
+   "STRINGLIST-REQUEST"
+   "<STRINGLIST-RESPONSE>"
+   "STRINGLIST-RESPONSE"
+  ))
+
